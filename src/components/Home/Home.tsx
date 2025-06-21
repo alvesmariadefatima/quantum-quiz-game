2a0f204dc8666249532b10291406dba3e2d275e6
Maria de Fátima <mnunesalves334@gmail.com>
2025-06-21 14:37:51 -0300

    feat: Inserir rota para que o usuário seja redirecionado para a tela nickname ao clicar no botãp iniciar partida

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,24 @@
 import Footer from "../Footer/Footer"
+import { useNavigate } from "react-router-dom"
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleStartGame = () => {
+    localStorage.setItem("gameStarted", "true");
+    navigate("/nickname");
+  };
+
     return (
         <>
         <div className="flex items-center justify-center min-h-screen">
             <section className="bg-white shadow-lg rounded-xl p-6 w-80 text-center">
             <h1 className="text-xl font-semibold mb-4">⚛️ Quantum Quiz</h1>
     
-            <button className="bg-purple-400 text-white text-sm px-4 py-2 rounded w-full mb-2">
+            <button
+                onClick={handleStartGame}
+                className="bg-purple-400 hover:bg-purple-500 text-white text-sm px-4 py-2 rounded w-full mb-3 cursor-pointer transition duration-200"
+            >
                 Iniciar Partida
             </button>
     
