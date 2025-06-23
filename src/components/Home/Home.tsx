f673e7b28f23c77874272e33d82347817ad5e164
Maria de Fátima <mnunesalves334@gmail.com>
2025-06-23 13:22:22 -0300

    feat: Adicionar redirecionamento de rota e também na propriedade onClick no botão Regras do Jogo

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,14 +5,17 @@ const Home = () => {
     const navigate = useNavigate();
 
     const handleStartGame = () => {
-    localStorage.setItem("gameStarted", "true");
-    navigate("/nickname");
-  };
+        navigate("/nickname");
+    };
+
+    const handleGameInstructions = () => {
+        navigate("/gameinstructions");
+    };
 
     return (
         <>
         <div className="flex items-center justify-center min-h-screen">
-            <section className="bg-white shadow-lg rounded-xl p-6 w-80 text-center">
+            <section className="bg-white shadow-lg rounded-xl p-6 w-90 text-center">
             <h1 className="text-xl font-semibold mb-4">⚛️ Quantum Quiz</h1>
     
             <button
@@ -22,7 +25,10 @@ const Home = () => {
                 Iniciar Partida
             </button>
     
-            <button className="bg-purple-400 text-white text-sm px-4 py-2 rounded w-full">
+            <button 
+                onClick={handleGameInstructions}
+                className="bg-purple-400 hover:bg-purple-500 text-white text-sm px-4 py-2 rounded w-full cursor-pointer"
+            >
                 Regras do Jogo
             </button>
         </section>
