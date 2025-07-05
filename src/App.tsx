a67cefd1f8f17b5b006e8eaaed4a778acf02c375
Maria de Fátima <mnunesalves334@gmail.com>
2025-07-05 12:03:58 -0300

    feat: Adicionar rota para a tela QuizPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import InsertNickname from './components/InsertNickname/InsertNickname';
 import GameInstructions from './components/GameInstructions/GameInstructions';
+import QuizPage from './components/QuizPage/QuizPage';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/nickname" element={<InsertNickname />} />
           <Route path="/gameinstructions" element={< GameInstructions/>} />
+          <Route path="/quiz" element={<QuizPage />} />
         </Routes>
       </BrowserRouter>
     </div>
