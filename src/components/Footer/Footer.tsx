2f6beb2968a9b403271df0c4efb9b5a64537bcb9
Maria de Fátima <mnunesalves334@gmail.com>
2025-10-10 11:10:06 -0300

    feat: Alteração no conteúdo do footer para garantir direitos autorais

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,14 +3,14 @@ const Footer = () => {
         <>
         <footer className="bg-white px-4 py-6 w-full text-center shadow-md">
             <p className="text-sm sm:text-base leading-relaxed">
-                Desenvolvido com 💜 por{" "}
+                Feito com 💜 e carinho por{" "}
                 <a
                     href="https://github.com/alvesmariadefatima"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-purple-600 underline break-words"
                 >
-                    Maria de Fátima Nunes Alves
+                    @alvesmariadefatima
                 </a>
             </p>
         </footer>
